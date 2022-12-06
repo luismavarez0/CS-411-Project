839f5304815402ffbafa48f0cfcfc6075a01b97e
mtahliani <milantahliani01@gmail.com>
2022-12-06 12:28:40 -0500

    authentication

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+import json
 
 app = Flask(__name__)
 
@@ -7,9 +9,18 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @app.route("/signin")
 def handle_signin():
-    return "this is for sign in"
+    user_data = json.loads(request.data)
+    user_name = user_data["name"]
+    user_password = user_data["password"]
+    with open("./userInfo.json", "r") as f:
+        user_file = json.load(f)
+        if user_name not in user_file.keys():
+            return "use does not exist"
+        else:
+            return user_name if user_file[user_name] == user_password else "wrong password"
 
 
 if __name__ == '__main__':
