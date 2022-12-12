adfbe1ed83916c64f03505d950cdfaf7b8a55a4a
mtahliani <milantahliani01@gmail.com>
2022-12-12 16:49:07 -0500

    EVERYTHING DONE FRONTEND

--- frontend/src/components/authentication/flights.jsx
+++ frontend/src/components/authentication/flights.jsx
@@ -1,49 +1,69 @@
-import React from "react";
-import axios from 'axios';
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import NavigateButton from "./Navigate";
+import Inputs from "./Inputs";
+import axios from "axios";
+import { v4 as uuidv4 } from 'uuid';
 
 const Flights = () => {
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '1418f00a1cmsh0b0a8ff349c39a5p1d1310jsn01479a90c739',
-// 		'X-RapidAPI-Host': 'skyscanner44.p.rapidapi.com'
-// 	}
-  
-// };
+  const navigate = useNavigate();
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+  const [foundFlight, setFoundFlight] = useState([]);
+  const flightDisplayStyle = "basis-1/5 mx-2"
 
-// fetch('https://skyscanner44.p.rapidapi.com/search-extended?adults=1&origin=MUC&destination=BER&departureDate=2022-10-11&currency=EUR&stops=0%2C1%2C2&duration=50&startFrom=00%3A00&arriveTo=23%3A59&returnStartFrom=00%3A00&returnArriveTo=23%3A59', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '1418f00a1cmsh0b0a8ff349c39a5p1d1310jsn01479a90c739',
-      'X-RapidAPI-Host': 'skyscanner44.p.rapidapi.com'
+  const handleOrigin = (event) => {
+    setOrigin(event.target.value);
+  }
+
+  const handleDestination = (event) => {
+    setDestination(event.target.value);
+  }
+
+  const findFlights = async () => {
+    const config = {
+      method: 'get',
+      url: `http://127.0.0.1:5000/getflight?origin=${origin}&destination=${destination}`,
+      headers: { }
+    };
+    try {
+      const res = await axios(config);
+      setFoundFlight(res.data);
+    } catch (e) {
+      console.log(e);
     }
   }
-  const params = new URLSearchParams({
-    adults: '1',
-    origin: 'BOS',
-    destination: 'LGA',
-    departureDate: '2022-12-15',
-    returnDate: '2022-12-17',
-    currency: 'EUR',
-    stops: '0,1,2',
-    duration: '50',
-    startFrom: '00:00',
-    arriveTo: '23:59',
-    returnStartFrom: '00:00',
-    returnArriveTo: '23:59'
-  })
-  
-  const url = `https://skyscanner44.p.rapidapi.com/search-extended?${ params.toString() }`
-  console.log(url)
+  return(
+      <div className={"text-center bg-slate-500 h-screen mx-auto border-2 flex flex-col border-gray-700 rounded-lg"}>
+        <h1>Enter your origin followed by your destination below:</h1>
+        <Inputs placeHolder={"origin"} funcCall={handleOrigin} ></Inputs>
+        <Inputs placeHolder={"destination"} funcCall={handleDestination}></Inputs>
+        <NavigateButton buttonFunction={findFlights} buttonText={"Click here to find flights"}></NavigateButton>
 
-  fetch(url, options)
-	  .then(response => response.json())
-	  .then(response => console.log(response))
-	  .catch(err => console.error(err));
-};
+
+        <div className={"h-screen border-2 border-gray-700 rounded-lg w-2/3 mx-auto"}>
+          <div className={"flex"}>
+            <span className={flightDisplayStyle}>arrivalAirportCode</span>
+            <span className={flightDisplayStyle}>arrivalTime</span>
+            <span className={flightDisplayStyle}>departureAirportCode</span>
+            <span className={flightDisplayStyle}>departureTime</span>
+            <span className={flightDisplayStyle}>price</span>
+          </div>
+          <hr />
+          {foundFlight.map((each, index) => {
+            return (
+                <div className={"flex"} key={uuidv4()}>
+                  <span className={flightDisplayStyle} key={uuidv4()}>{each.arrivalAirportCode}</span>
+                  <span className={flightDisplayStyle} key={uuidv4()}>{each.arrivalTime}</span>
+                  <span className={flightDisplayStyle} key={uuidv4()}>{each.departureAirportCode}</span>
+                  <span className={flightDisplayStyle} key={uuidv4()}>{each.departureTime}</span>
+                  <span className={flightDisplayStyle} key={uuidv4()}>{each.price}</span>
+                </div>
+            )
+          })}
+        </div>
+      </div>
+  )
+}
 
 export default Flights;
